46ddbdc242c384566f8dd94156c94c030cf5aac3
shaymaebkn <chaymaeboukhana.solicode@gmail.com>
2022-03-04 16:10:41 +0100

    save

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 
     include_once 'connect/connect.php';
@@ -23,48 +23,6 @@
     }
     
 ?>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIGN-UP</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
 
-<form>
-  <div class="form-group">
-    <label for="firstname">FIRST NAME</label>
-    <input type="text" class="form-control" id="firstname"  >
 
-    <label for="lastname">LAST NAME</label>
-    <input type="text" class="form-control" id="lasttname"  >
-   
-    <label for="adress">ADRESS</label>
-    <input type="text" class="form-control" id="adress"  >
 
-    <label for="phone">PHONE NUMBER</label>
-    <input type="text" class="form-control" id="phone"  >
-
-    <label for="email">EMAIL</label>
-    <input type="email" class="form-control" id="email"  >
-
-    <label for="password">PASSWORD</label>
-    <input type="number" class="form-control" id="password"  >
-
-    <label for="password">CONFIRM PASSWORD</label>
-    <input type="number" class="form-control" id="password"  >
-      
-  </div>
-
-  <button type="submit" class="btn btn-primary">SIGN up</button>
-
-</form>
-
-</body>
-
-</html>
->>>>>>> bff672dcba285ac6fc366e61c3efcc1720307d75
