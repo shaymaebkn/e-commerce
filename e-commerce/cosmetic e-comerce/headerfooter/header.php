79d316e01bc40d1b211efdce63f90674630e7c7e
shaymaebkn <92023318+shaymaebkn@users.noreply.github.com>
2022-03-04 12:32:59 +0000

    Add files via upload

---
+++ e-commerce/cosmetic e-comerce/headerfooter/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> <!-- bootstrap -->
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav class="nav1">
+            <div class="burger" id="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <div class="nav_links">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li id="catalogue"><a href="#">CATALOGUE</a></li>
+                    <li><a href="#">SIGN-UP</a></li>
+                </ul>
+            </div>
+            <div class="logo" id="logo">
+                <a href="#"><img src="pics/logo.png" alt="paon" width="160px"></a>
+            </div>
+            <div class="icons" mt-2>
+                <a href="#"><img src="pics/search.png"></a>
+                <a href="#"><img src="pics/cart.png"></a>
+                <a href="#"><img src="pics/profil-de-lutilisateur.png"></a>
+            </div>
+        </nav>
+        <nav class="nav2" id="nav2">
+            <ul>
+                <li><a href="#">skin care</a></li>
+                <li><a href="#">make-up</a></li>
+                <li><a href="#">perfume</a></li>
+                <li><a href="#">bath and body</a></li>
+                <li><a href="#">hair</a></li>
+                <li><a href="#">accessories</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ e-commerce/cosmetic e-comerce/index.php
@@ -0,0 +1,169 @@
+
+<?php
+include 'headerfooter/header.php';
+?>
+    <section>
+    <div class="container ">
+        <div >
+            <img style="width: 100%;" src="pics/sikin care.jpg" alt="">
+        </div>
+        <div class="d-flex justify-content-center p-3">
+            <img class="p-3" style="width: 35%;" src="pics/cosmetic men.JPG" alt="">
+            <img class="p-3" style="width: 35%;" src="pics/cosmetic kids.jpg" alt="">
+            <img class="p-3" style="width: 35%;" src="pics/cosmetic women.jpg" alt="">
+        </div>
+
+    </div>
+    </section>
+    <section>
+        <div>
+            <h2></h2>
+        </div>
+        <div class="row justify-content-center">
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+        
+
+        </div>
+    </section>
+
+    <section>
+        <div>
+            <h2></h2>
+        </div>
+        <div class="row justify-content-center">
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+        
+
+        </div>
+    </section>
+
+    <section>
+        <div>
+            <h2></h2>
+        </div>
+        <div class="row justify-content-center">
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+        
+
+        </div>
+    </section>
+
+    <section>
+        <div>
+            <h2></h2>
+        </div>
+        <div class="row justify-content-center">
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+        
+
+        </div>
+    </section>
+
+   <?php
+   include 'headerfooter/footer.php';
+  
+   ?>
