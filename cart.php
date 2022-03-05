6b671541b19b0f659aef59382c7198ca74626b32
ahadout <mohamedahdoutt@gmail.com>
2022-03-05 17:29:47 +0000

    save

--- cart.php
+++ cart.php
@@ -1,5 +1,6 @@
 <?php
-include 'headerfooter/header.php';
+    include 'headerfooter/header.php';
+
 ?>
 <main>
     <section class="section1">
@@ -12,17 +13,29 @@ include 'headerfooter/header.php';
                 <h1>CART :</h1>
             </div>
             <div class="cart_table">
-                <table>
+                <table class="table">
                     <thead>
                         <th>Product</th>
                         <th>Price/un</th>
                         <th>QTE</th>
                         <th>Total</th>
                     </thead>
-                    <tbody></tbody>
+                    <tbody>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tbody>
+                    <tfoot>
+                        <td colspan="3" align="center">Subtotal before delevry charges</td>
+                        <td>Total</td>
+                    </tfoot>
                 </table>
             </div>
-            
+            <div class="next">
+                <button class="next_button">Next</button>
+            </div>
+
     </section>
 </main>
 
