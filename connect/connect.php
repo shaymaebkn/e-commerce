2094084bc4916337003123d156d4cca86203b908
faisalbazid <faisal.bazid.solicode@gmail.com>
2022-03-05 18:38:08 +0100

    commit

--- connect/connect.php
+++ connect/connect.php
@@ -1,22 +1,34 @@
 <?php
 
-$password = "";
-$user = "root";
-$name_bd = "e-commerce";
+// $password = "";
+// $user = "root";
+// $name_bd = "e-commerce";
 
-try {
-	$bd = new PDO (
-		'mysql:host=localhost;
-		dbname='.$name_bd,
-		$user,
-		$password,
-		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-	);
-} catch (Exception $e) {
-	echo "there is a problem: ".$e->getMessage();
+// try {
+// 	$bd = new PDO (
+// 		'mysql:host=localhost;
+// 		dbname='.$name_bd,
+// 		$user,
+// 		$password,
+// 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+// 	);
+// } catch (Exception $e) {
+// 	echo "there is a problem: ".$e->getMessage();
+// }
+
+$con=new mysqli('localhost','root','','database');
+
+if($con){
+    
+     echo 'connection is working';
+    
+}
+else{
+    die(mysqli_error($con));
 }
 
 
-?>
 
-    
+?> 
+
+
