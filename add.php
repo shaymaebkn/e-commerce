85b7b0a28435f0fbd96f22e29d768178bbb3a662
shaymaebkn <chaymaeboukhana.solicode@gmail.com>
2022-03-08 15:28:59 +0100

    save

--- add.php
+++ add.php
@@ -15,8 +15,27 @@
     $product = $statement->fetch(PDO::FETCH_OBJ);
     $data = $product;
     //print_r($persona);
+   
+
+ 
+ if(isset($_POST['add'])){
+     
+    include_once 'login/connect.php';
+    $sql="SELECT * FROM `customers` WHERE customerCode= ?;";
+    $result=mysqli_query($con,$sql);
+
+    $customercode = $_SESSION['customerCode'] ;
+    $quantity = $_POST['quantity'];
+    $id = $_GET['id'];
+
+    $statement = $bd->prepare("INSERT INTO `orders`(`customerCode`, `orderDate`, `deliveryAddress`) VALUES (?,?,?);");
+    $result = $statement->execute([$id,$quantity]);
+ }
+
+
     
-?>
+    
+    ?>
 <body>
             <div class="row col-3 p-3 ">
                 <div class="card " style="width: 20rem;">
@@ -24,12 +43,14 @@
                     <div class="card-body">
                      <h4 class="text-center" class="card-text"><?php echo $data->productName; ?></h4>   
                     <h6 class="text-center"><?php echo number_format($data->unitPrice); ?>DH</h6>
-                    
                     <p class="text-center"> <?php echo $data->description; ?></p>
-                    <a >
-                    <button><a href="">add to cart</a></button>
-                    </svg>
-                    </a>
+
+                    <form action="insertcard.php?id=<?php echo $data->productID; ?>" method="POST">
+                    <input type="number" name="quantity">
+                    <div class="d-grid">
+                        <input type="submit" name="add" class="btn btn-primary" value="add to card">
+                    </div>
+                    </form>
                     </div>
                 </div>
            </div>
---
+++ insertcard.php
@@ -0,0 +1,21 @@
+<?php 
+//  include_once 'connect/connect.php';
+ 
+//  if(isset($_POST['add'])){
+
+//     $quantity = $_POST['quantity'];
+//     $id = $_GET['id'];
+
+//     $statement = $bd->prepare("INSERT INTO `orderdetails`( `productID`, `orderedQuantity`) VALUES (?,?);");
+//     $result = $statement->execute([$id,$quantity]);
+//  }
+?>
+<!-- //  if ($result === TRUE) {
+//      header('Location: index.php?mensaje=registrado');
+//  } else {
+//      header('Location: index.php?mensaje=error');
+//      exit();
+//  }
+
+//  } -->
+
