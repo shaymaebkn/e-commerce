705f32d1d98e037d403f2cd82e621befd01ea781
ahadout <mohamedahdoutt@gmail.com>
2022-03-08 12:12:35 +0100

    save

---
+++ connect/myconnect.php
@@ -0,0 +1,8 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "Database_e-commerce";
+
+error_reporting(0);
+$connect = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
