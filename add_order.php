f51d36bfdfb552ca0552624d84ecfa3b16bab623
ahadout <mohamedahdoutt@gmail.com>
2022-03-13 02:13:51 +0000

    save

--- add_order.php
+++ add_order.php
@@ -1,25 +1,27 @@
 <?php
+    session_start();
     include 'connect/myconnect.php';
     $customerCode = $_SESSION['customerCode'];
     $orderDate = date("Y-m-d");
     $deliveryAddress = $_SESSION['adress'];
 
 
-    $sql = "INSERT INTO orders (customerCode, orderDate, deliveryAddress) VALUES ('$customerCode', '$orderDate', '$deliveryAddress';)";
+    $sql = "INSERT INTO orders (customerCode, orderDate, deliveryAddress) VALUES ('$customerCode', '$orderDate', '$deliveryAddress');";
     mysqli_query($connect, $sql);
 
-    $sql2 = "SELECT orderID FROM orders WHERE customerCode='$customerCode';";
+    $sql2 = "SELECT  orderID FROM orders order by orderID desc limit 1;";
     $result = mysqli_query($connect, $sql2);
     $resultCheck = mysqli_num_rows($result);
-    if ($resultCheck > 0){
-        $row = mysqli_fetch_assoc($result);
-        $orderID = $row[count($row)-1];
+    if($resultCheck > 0){
+        $row = mysqli_fetch_assoc($result) ;
+        $orderID = $row['orderID'];
     }
     for($i=0; $i<count($_SESSION['cart']); $i++){
         $productID = $_SESSION['cart'][$i][0];
         $orderedQuantity = $_SESSION['cart'][$i][1];
-        $sqlX = "INSERT INTO orderDetails (orderID, productID, orderedQuantity) VALUES ('$orderID', '$productID', '$orderedQuantity')";
-        mysqli_query($connect, $sqlX);
+        $sql3 = "INSERT INTO orderDetails (orderID, productID, orderedQuantity) VALUES ('$orderID', '$productID', '$orderedQuantity');";
+        mysqli_query($connect, $sql3);
     }
+    session_unset();
     header("Location : confirmed_order.php");
 ?>
